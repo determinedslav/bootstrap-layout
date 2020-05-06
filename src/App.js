18fce751cfc724876eea763c7586d26d8e0fbc61
DeterminedSlav <slavimitev10@gmail.com>
2020-05-06 18:30:22 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 import './App.css';
 
@@ -11,6 +12,7 @@ const Layout = props => {
       <div className="container pt-5">
         {props.children}
       </div>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Footer = () => {
+
+  return (
+    <div className="container border-top border-secondary">
+        <div className="row">
+            <div className="col-2">penis</div>
+            <div className="col-2">penis</div>
+            <div className="col-2">penis</div>
+            <div className="col-2">penis</div>
+            <div className="col-2">penis</div>
+            <div className="col-2">penis</div>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
