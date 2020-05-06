41f448e7848761a4de228937724af1bbf0658903
DeterminedSlav <slavimitev10@gmail.com>
2020-05-06 18:15:30 +0300

    removed commented code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,8 @@
 import React, {useState} from 'react';
 import Button from '../components/Button'
 import CategoryCard from '../components/CategoryCard'
-//import {useSelector, useDispatch} from "react-redux";
-//import { useHistory } from "react-router-dom";
 
 const Home = () => {    
-
-    /*
-    //getters/setters
-    const [region, setRegion] = useState(' ');
-    const [regionFull, setRegionFull] = useState(' ');
-    const [name, setName] = useState(' ');
-    const [errorMessage, setErrorMessage] = useState(' ');
-
-    const [matches] = useState([]);
-
-    const isLoading = useSelector(state => state.loading);
-
-    const dispatch = useDispatch();
-    const history = useHistory();
-    */
-
-    //Home page render
     return <div>
         <div className="row">
             <div className="col">
