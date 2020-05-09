5bfb5f4ab64771100c6dfe497e8505a5ed3afc88
DeterminedSlav <slavimitev10@gmail.com>
2020-05-09 18:43:59 +0300

    minor design change

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryItem = props => {
 
   return (
-    <a className="text-secondary mr-3 mb-2" href="#">
+    <a className="text-secondary ml-3 mb-2" href="#">
       <div className={"text-" +props.iconColor+ " h4 border-0 rounded-lg bg-dark p-3"}>
         {props.icon}                
       </div>
