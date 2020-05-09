6cbd6325de7b1b8885c6ae03125b582fe97002f3
DeterminedSlav <slavimitev10@gmail.com>
2020-05-09 17:11:21 +0300

    minor design change

--- src/components/FooterSocialItem.js
+++ src/components/FooterSocialItem.js
@@ -3,8 +3,8 @@ import React from 'react';
 const footerSocialItem = props => {
 
   return (
-    <div className="pr-2 pb-2">
-        <a className="border-0 rounded-circle bg-dark text-white pt-1 pb-1 pl-2 pr-2" href="#">
+    <div className="pr-2 pb-2 mb-2">
+        <a className="h5 border-0 rounded-circle bg-dark text-white pt-1 pb-1 pl-2 pr-2" href="#">
           {props.icon}
         </a>
     </div>
